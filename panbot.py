4512aa2394e306931a69ad1bbd7f0f4f05945281
Goderich <yuhsien77@gmail.com>
2021-07-10 20:19:58 +0800

    Add first version of bot

---
+++ panbot.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import discord
+from panbot_search import select_candidates
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+GUILD = os.getenv('GUILD')
+client = discord.Client()
+
+help_text = ''' Welcome to **PANBot**!
+To use the bot, type in /pan followed by your query:
+> /pan dog
+
+You can also search for multiple items at the same time:
+> /pan sun moon
+
+To search for a specific string instead of a list of words, use quotes:
+> /pan "a dog"
+
+You can also search PAn protoforms!
+To do this, begin your query with an asterisk (*):
+> /pan *num
+Note that this does a partial search of all protoforms containing "num" (or "Num", it's not case sensitive!), not just protoforms that start with "num".
+
+If your protoform query contains at least one uppercase character, it will become case sensitive. E.g.:
+> /pan *Num
+This will return all protoforms containing "Num", but not "num".
+
+Have fun!
+'''
+
+@client.event
+async def on_ready():
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '/pan test':
+        response = 'Panbot operational!'
+        await message.channel.send(response)
+        return
+
+    if message.content.strip() in ['/pan help', '/pan ?']:
+        await message.channel.send(help_text)
+        return
+
+    if message.content.startswith('/pan '):
+        query = message.content.removeprefix('/pan ')
+        response = select_candidates(query)
+        if len(response) > 10:
+            await message.channel.send(
+            'Too many results! Try specifying a narrower query.')
+        else:
+            for item in response:
+                await message.channel.send(item)
+            return
+
+
+client.run(TOKEN)
---
+++ panbot_search.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import csv
+
+def filter_candidates_string(query):
+    with open('pan.csv', newline='') as f:
+        reader = csv.reader(f)
+        candidates = [row for row in reader if query in row[2]]
+    perfect_matches = [c for c in candidates if c[2] == query]
+    if perfect_matches:
+        return perfect_matches
+    elif candidates:
+        return candidates
+
+def filter_candidates_proto(query):
+    case_sensitive = query.islower()
+    with open('pan.csv', newline='') as f:
+        reader = csv.reader(f)
+        if case_sensitive:
+            candidates = [row for row in reader if query in row[0].lower()]
+        else:
+            candidates = [row for row in reader if query in row[0]]
+    return candidates
+
+def filter_candidates_wordlist(query):
+    wordlist = query.split()
+    with open('pan.csv', newline='') as f:
+        reader = csv.reader(f)
+        candidates = [row for row in reader
+                      if any(w in row[2].split()
+                             for w in wordlist)]
+    return candidates
+
+def display_candidates(cs):
+    return [f'PAn **{c[0]}** "{c[2]}" https://www.trussel2.com/acd/{c[1]}'
+            for c in cs]
+
+def select_candidates(query):
+    if query.startswith('"') and query.endswith('"'):
+        candidates = filter_candidates_string(query.strip('"'))
+    elif query.startswith('*'):
+        candidates = filter_candidates_proto(query.lstrip('*'))
+    else:
+        candidates = filter_candidates_wordlist(query)
+
+    if candidates:
+        return display_candidates(candidates)
+    else:
+        return ["No matches found!"]
