11c3310b67ea615a56794cd5ebe7ccb63b44a27a
Walis Taywang <davids0ng0914@gmail.com>
2021-07-11 17:07:38 +0800

    5

--- foxbot.py
+++ foxbot.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-GUILD = os.getenv('GUILD')
+TOKEN = os.environ["TOKEN"]
+GUILD = os.environ["GUILD"]
 client = discord.Client()
 
 help_text = ''' Welcome to **PANBot**!
