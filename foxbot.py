86734ee10d3f7c88acddf7e518c1e663ba318a5a
Walis Taywang <davids0ng0914@gmail.com>
2021-07-11 19:34:34 +0800

    Update foxbot.py

--- foxbot.py
+++ foxbot.py
@@ -13,31 +13,39 @@ GUILD= os.environ["GUILD"]
 # GUILD = os.getenv('GUILD')
 client = discord.Client()
 
-help_text = ''' Welcome to **PANBot**!
-To use the bot, type in /pan followed by your query:
-> /pan dog
+help_text = '''
+for help:
+> /fox help
 
-You can also search for multiple items at the same time:
-> /pan sun moon
-
-To search for a specific string instead of a list of words, use quotes:
-> /pan "a dog"
-
-You can also search PAn protoforms!
-To do this, begin your query with an asterisk (*):
-> /pan *num
-Note that this does a partial search of all protoforms containing "num" (or "Num", it's not case sensitive!), not just protoforms that start with "num".
-
-If your protoform query contains at least one uppercase character, it will become case sensitive. E.g.:
-> /pan *Num
-This will return all protoforms containing "Num", but not "num".
-
-PMP search is also available!
-Just start your query with /pmp
-Just like PAn, you can use unquoted word lists, quoted strings, and protoforms with a preceding asterisk (*).
-
-Have fun!
+for Seediq:
+> /sdq 中文
 '''
+
+# ''' Welcome to **PANBot**!
+# To use the bot, type in /pan followed by your query:
+# > /pan dog
+#
+# You can also search for multiple items at the same time:
+# > /pan sun moon
+#
+# To search for a specific string instead of a list of words, use quotes:
+# > /pan "a dog"
+#
+# You can also search PAn protoforms!
+# To do this, begin your query with an asterisk (*):
+# > /pan *num
+# Note that this does a partial search of all protoforms containing "num" (or "Num", it's not case sensitive!), not just protoforms that start with "num".
+#
+# If your protoform query contains at least one uppercase character, it will become case sensitive. E.g.:
+# > /pan *Num
+# This will return all protoforms containing "Num", but not "num".
+#
+# PMP search is also available!
+# Just start your query with /pmp
+# Just like PAn, you can use unquoted word lists, quoted strings, and protoforms with a preceding asterisk (*).
+#
+# Have fun!
+# '''
 
 @client.event
 async def on_ready():
